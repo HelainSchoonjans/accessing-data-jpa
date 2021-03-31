3d92e878cc760b49aacb6000a2a2e07453384786
hschoonjans <hschoonjans@cirb.brussels>
2021-03-31 15:17:08 +0200

    First commit

---
+++ src/main/groovy/com/example/accessingdatajpa/AccessingDataJpaApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.accessingdatajpa
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AccessingDataJpaApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(AccessingDataJpaApplication, args)
+    }
+
+}
